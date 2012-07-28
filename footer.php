0a75a382e12373ffa5ab39da63710070bef3a47c
Piet Bos <info@senlinonline.com>
2012-07-28 16:55:13 +0800

    update footer.php
    replace link to theme

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
 				<p><a rel="nofollow" href="#wrap"><?php _e('Go to top ', 'basejump'); ?>&uarr;</a></p>
 			</div><!-- end .gototop -->
 			<div class="creds">
-				<p><?php _e('&copy; Copyright ', 'basejump'); ?><?php echo date('Y'); ?> &middot; <a href="<?php echo home_url(); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a> &middot; <a href="http://senl.in/baseJumP" title="<?php _e('Basejump WordPress Theme ', 'basejump'); ?>"><?php _e('Basejump WordPress Theme ', 'basejump'); ?></a><?php _e('developed by ', 'basejump'); ?><a href="http://senlinonline.com" title="<?php _e('Basejump WordPress Theme developed by Senlin Online', 'basejump'); ?>">Senlin Online</a> &middot; <?php _e('Powered by ', 'basejump'); ?><a href="http://wordpress.org" target="_blank" title="<?php _e('Powered by WordPress', 'basejump'); ?>"><?php _e('WordPress', 'basejump'); ?></a></p>	
+				<p><?php _e('&copy; Copyright ', 'basejump'); ?><?php echo date('Y'); ?> &middot; <a href="<?php echo home_url(); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a> &middot; <a href="http://wpbaseju.mp/" title="<?php _e('Basejump WordPress Theme ', 'basejump'); ?>"><?php _e('Basejump WordPress Theme ', 'basejump'); ?></a><?php _e('developed by ', 'basejump'); ?><a href="http://senlinonline.com" title="<?php _e('Basejump WordPress Theme developed by Senlin Online', 'basejump'); ?>">Senlin Online</a> &middot; <?php _e('Powered by ', 'basejump'); ?><a href="http://wordpress.org" target="_blank" title="<?php _e('Powered by WordPress', 'basejump'); ?>"><?php _e('WordPress', 'basejump'); ?></a></p>	
 			</div><!-- end .creds -->
 		</div><!-- end .wrap -->
 	</div><!-- end #footer -->
